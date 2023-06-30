1de6f984abaa5163ac2935f488a729c9d90a11c6
Tural Xanaliyev <turalxanaliyev14@gmail.com>
2023-06-30 22:58:02 +0400

    Lessons 2

---
+++ js-1/app/app.js
@@ -0,0 +1,70 @@
+// BOM və DOM obyektləri
+// BOM - Browser Object Model
+// DOM - Document Object Model
+
+// let,var və const 
+// ! Diger tiplər Float,Double,Int
+// const container = document.getElementById("div")
+// const input = document.getElementById("text")
+// let a=5
+// let a
+// a=2
+// var b
+// var b
+// const b=5
+// b=10;
+// let text = input.value
+// alert(text + " Salam")
+// let text = "Salam Dünya";
+// let number = 5;
+
+// ! Js-də type növləri:
+/* 
+? number,string,boolen(true,false),array(massiv),Obyekt(Obj),null,undefined, NaNm,
+? Primiteve : number,string,boolen(true,false),null,undefined, NaN-Not a Number
+? Non-Primiteve,Reference : array(massiv),Obyekt(Obj)
+*/
+// let boolen = null;
+// boolen = "1.5";
+// console.log(parseInt(boolen));
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// console.log(arr.forEach((item) => item));
+
+// arr.forEach((item) => console.log(item));
+
+// let name = "Elvin";
+// let surname = "Memmedov";
+// let age = 25;
+// let obj = {
+//     name:"Elvin",
+//     surname:"Memmedov",
+//     age:25,
+// }
+// console.log(name,surname,age)
+// obj.name = "Samir"
+// console.log(obj)
+
+// let obj2 = {
+//     name:"Samir",
+//     surname:"Memmedov",
+//     age:25,
+// }
+
+// let a = [1,2,3]
+// let b = [4,5,6]
+// let c = a;
+// a=b;
+// b=[7] 
+// console.log(a,b,c)
+
+// Promt,Confirm,Alert
+
+// let i = prompt("salam","1-5 arasinda qiymet yaz")
+// console.log(i)
+
+// confirm()
+
+// alert("Xos geldiniz")
+
---
+++ js-2/app.js
@@ -0,0 +1,82 @@
+// simple
+// referance
+
+// let a = [3, 5, 7];
+// let b=[...a];
+// a[0]=2
+// console.log(a,b);
+// let b = 5;
+// console.log(b**3)
+
+// ! String + number
+// let a = "1"
+// let b = 5;
+
+// console.log("a+b=",a+b)
+// console.log("b-a=",b-a)
+
+// ! 1) if,else if,else
+// ! 2) parms ? {} : {}
+// ! 3) swich case
+
+// ? Həm qiymətinə görə həmdə tipinə görə eyni olduğunu yoxlamaq üçün ===
+// ? Yanlız qiymətini yoxlmaq üçün isə ==
+
+// if (a < 10) {
+//   console.log("a kicikdir 10 dan");
+// } else if (a === 10) {
+//   console.log("a berbaberdi 10-na");
+// }  else if (a == 15) {
+//   console.log("a beraberdi 15-e");
+// }else {
+//   console.log("verilen sertleri odemir");
+// }
+
+// ? Inkar operatoru(NOT) ! 1=0,0=1;true=false,false=true
+// let a = 1;
+
+// if(a <= 10){
+//   console.log("a 10dan ferqlidir")
+// }else{
+//   console.log("a 10 na beraberdir")
+// }
+
+// ? Eyni vaxtda 2 sert vermek
+// ? && - AND(və) true<->true = true, true<->false = false,false<->true=false, false<->false = false
+// ? && - AND(və) 1 <-> 1 = 1,1<->0 = 0,0<->1 = 0,0<->0 = 0
+// let a = 15;
+// if (a >= 10 && a <= 15  && a!==0) {
+//   console.log("a 10 dan boyukdur 15 ise kicik")
+// }else{
+//   console.log("a ne 10 ile 15 araqlinda deyil")
+// }
+
+// ? Ən az 1 şərt ödənsin
+// ? || - OR(və ya)  true<->true = true, true<->false = true,false<->true=true, false<->false = false
+// ? || - OR(və ya)  1 <-> 1 = 1,1<->0 = 1,0<->1 = 1,0<->0 = 0
+
+// let b = 12
+
+// if(b<15 || b>30){
+//   console.log("b 15 den kicikdir")
+// }else{
+//   console.log("b 15 ile 30 aralqindadir")
+// }
+// let a = 9;
+// a < 10 ? (console.log("a 10 dan kicikdir")) : (console.log("a 10dan boyukdur"));
+
+// ! alert, prompt, confirm
+
+
+
+if (confirm("Admin kimi daxil olmaq isteyirsiniz? ")) {
+  let login = prompt("parolu daxil edin");
+  if(Number(login) === 1234){
+    alert("xos geldin admin")
+  }else{
+    alert("Parol yanlisidr")
+  }
+} else {
+  let user = prompt("Adinizi yazin")
+  alert(`${user} xos geldin`)
+}
